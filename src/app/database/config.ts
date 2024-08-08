38d638376f878416fcccff5e44424ad9d543d47f
Erick Icaza <erick_icaza_94@hotmail.com>
2024-08-08 15:14:34 -0500

    changes 9

--- src/app/database/config.ts
+++ src/app/database/config.ts
@@ -5,11 +5,6 @@ const USER = process.env.MYSQLUSER || "root";
 const PORT = process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306;
 const PASSWORD = process.env.MYSQLPASSWORD || "barcelo1994";
 const DATABASE = process.env.MYSQLDATABASE || "mydb";
+const URL = process.env.MYSQL_PUBLIC_URL || "";
 
-export const connection = await createConnection({
-  host: HOST,
-  user: USER,
-  port: PORT,
-  password: PASSWORD,
-  database: DATABASE,
-});
+export const connection = await createConnection(URL);
