88411806ca6599e4f3fe83b5a3d6f35dbd6a3897
Erick Icaza <erick_icaza_94@hotmail.com>
2024-08-08 12:27:14 -0500

    Changes 7

--- src/app/database/config.ts
+++ src/app/database/config.ts
@@ -1,10 +1,10 @@
 import { createConnection } from "mysql2/promise";
 
-const HOST = process.env.DB_HOST || "127.0.0.1";
-const USER = process.env.DB_USER || "root";
-const PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
-const PASSWORD = process.env.DB_PASSWORD || "barcelo1994";
-const DATABASE = process.env.DB_NAME || "mydb";
+const HOST = process.env.MYSQLHOST || "127.0.0.1";
+const USER = process.env.MYSQLUSER || "root";
+const PORT = process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306;
+const PASSWORD = process.env.MYSQLPASSWORD || "barcelo1994";
+const DATABASE = process.env.MYSQLDATABASE || "mydb";
 
 export const connection = await createConnection({
   host: HOST,
