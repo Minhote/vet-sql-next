fa084c92ccfe704882cf97b9144ca4fecc0fd28c
Erick Icaza <erick_icaza_94@hotmail.com>
2024-08-19 15:36:38 -0500

    URL 3

--- src/app/database/config.ts
+++ src/app/database/config.ts
@@ -9,16 +9,16 @@ const URL =
   `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.MYSQL_DATABASE}
 ` || null;
 
-const URL2 = "mysql://root:@mysql.railway.internal:3306/railway";
+const URL2 = "mysql://root:@roundhouse.proxy.rlwy.net:14553/railway";
 
-export const connection = URL
-  ? await createConnection(URL)
-  : await createConnection({
-      host: HOST,
-      user: USER,
-      port: PORT,
-      password: PASSWORD,
-      database: DATABASE,
-    });
+// export const connection = URL
+//   ? await createConnection(URL)
+//   : await createConnection({
+//       host: HOST,
+//       user: USER,
+//       port: PORT,
+//       password: PASSWORD,
+//       database: DATABASE,
+//     });
 
-// export const connection = await createConnection(URL2);
+export const connection = await createConnection(URL2);
