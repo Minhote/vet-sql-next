3e5b7d0b0bc7f6e074b1d8da840c449a370e6af3
Erick Icaza <erick_icaza_94@hotmail.com>
2024-08-19 15:30:23 -0500

    Database URL Changed

--- src/app/database/config.ts
+++ src/app/database/config.ts
@@ -6,19 +6,19 @@ const PORT = process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306;
 const PASSWORD = process.env.MYSQLPASSWORD || "barcelo1994";
 const DATABASE = process.env.MYSQLDATABASE || "mydb";
 const URL =
-  `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}` ||
-  null;
+  `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.MYSQL_DATABASE}
+` || null;
 
 const URL2 = "mysql://root:@mysql.railway.internal:3306/railway";
 
-// export const connection = URL
-//   ? await createConnection(URL)
-//   : await createConnection({
-//       host: HOST,
-//       user: USER,
-//       port: PORT,
-//       password: PASSWORD,
-//       database: DATABASE,
-//     });
+export const connection = URL
+  ? await createConnection(URL)
+  : await createConnection({
+      host: HOST,
+      user: USER,
+      port: PORT,
+      password: PASSWORD,
+      database: DATABASE,
+    });
 
-export const connection = await createConnection(URL2);
+// export const connection = await createConnection(URL2);
