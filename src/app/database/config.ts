23c7643a6b9e1fdb677930daf51cbbdb86fd8884
Erick Icaza <erick_icaza_94@hotmail.com>
2024-08-19 15:12:20 -0500

    Database Config Changed

--- src/app/database/config.ts
+++ src/app/database/config.ts
@@ -5,14 +5,16 @@ const USER = process.env.MYSQLUSER || "root";
 const PORT = process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306;
 const PASSWORD = process.env.MYSQLPASSWORD || "barcelo1994";
 const DATABASE = process.env.MYSQLDATABASE || "mydb";
-// const URL = process.env.MYSQL_PUBLIC_URL || "";
+const URL =
+  `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}` ||
+  null;
 
-// export const connection = await createConnection(URL);
-
-export const connection = await createConnection({
-  host: HOST,
-  user: USER,
-  port: PORT,
-  password: PASSWORD,
-  database: DATABASE,
-});
+export const connection = URL
+  ? await createConnection(URL)
+  : await createConnection({
+      host: HOST,
+      user: USER,
+      port: PORT,
+      password: PASSWORD,
+      database: DATABASE,
+    });
