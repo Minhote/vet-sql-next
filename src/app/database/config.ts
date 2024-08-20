0d97cae44f1aa490c4963e98ed48bea2b6b2dce5
Erick Icaza <erick_icaza_94@hotmail.com>
2024-08-20 10:17:43 -0500

    Mysql url edited

--- src/app/database/config.ts
+++ src/app/database/config.ts
@@ -1,13 +1,11 @@
 import { createConnection } from "mysql2/promise";
 
-const HOST = process.env.MYSQLHOST || "127.0.0.1";
-const USER = process.env.MYSQLUSER || "root";
-const PORT = process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306;
-const PASSWORD = process.env.MYSQLPASSWORD || "barcelo1994";
-const DATABASE = process.env.MYSQLDATABASE || "mydb";
-const URL =
-  `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}
-` || null;
+const HOST = "127.0.0.1";
+const USER = "root";
+const PORT = 3306;
+const PASSWORD = "barcelo1994";
+const DATABASE = "mydb";
+const URL = process.env.MYSQL_URL;
 
 export const connection = URL
   ? await createConnection(URL)
